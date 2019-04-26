73022d0bb29b1882fd98dd5e1e0cc2a1cd31a5f6
belikeking <belikeking.lee@gmail.com>
2019-04-26 17:22:12 +0900

    initialize

---
+++ granco-demo/src/main/java/kr/co/granco/demo/GrancoDemoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.granco.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrancoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrancoDemoApplication.class, args);
+	}
+
+}
